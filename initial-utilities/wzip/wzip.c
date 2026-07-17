0b279f2ae8773cd615c2d1af9f48f15b5ca065b4
Harsh <302592795+harshdotgit@users.noreply.github.com>
2026-07-18 01:20:54 +0400

    implement wzip

--- initial-utilities/wzip/wzip.c
+++ initial-utilities/wzip/wzip.c
@@ -1,17 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// read until next char is different.
-// if char is the first char:
-// 1. store char
-// 2. increment counter
-// if next char = stored char, increment counter
-// else,
-// 1. store number in int array, char into char array at same position
-// (maintains invariant of output = 5bytes)
-// 2. store new char
-// reset counter to 1
-
 int main(int argc, char **argv) {
   if (argc <= 1) {
     printf("wzip: file1 [file2 ...]\n");
